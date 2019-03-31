c17c5c6f8d322f402a75969074d28e7e62481fd4
Udyan Shardhar <udyancs@gmail.com>
2019-03-30 22:01:33 -0500

    Initial commit for autentcation application

---
+++ src/main/java/com/us/app/checkurity/CheckurityApplication.java
@@ -0,0 +1,13 @@
+package com.us.app.checkurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckurityApplication.class, args);
+	}
+
+}
